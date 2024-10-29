2351e1a3175e6a7f2bd8e7918dbf9fc40abe3b6e
Adwaith Jayasankar <kichu31084@gmail.com>
2024-10-29 14:57:02 +0530

    test4

--- src/pages/onboarding.tsx
+++ src/pages/onboarding.tsx
@@ -1,3 +1,5 @@
+'use client'
+import "../app/globals.css";
 import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import { useAuth } from '../hooks/useAuth';
