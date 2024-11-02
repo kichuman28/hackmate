0498312d4f0b8cd35c8c1b055a6af9e584370bfa
Adwaith Jayasankar <kichu31084@gmail.com>
2024-11-02 17:08:04 +0530

    Ignore: Succesfully built

--- dist/server/server-reference-manifest.js
+++ dist/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"svWlWJ6C6QUkbMnpiiwwzm4T55hXrvCu8hxRh+mgdpY=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
